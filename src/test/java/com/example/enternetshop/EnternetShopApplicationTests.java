5cd254d3af020ae082620c5ee338847e80d1ccdd
eralash911 <unreal725@gmail.com>
2022-12-06 00:30:38 +0300

    Initial commit

---
+++ src/test/java/com/example/enternetshop/EnternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enternetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnternetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
